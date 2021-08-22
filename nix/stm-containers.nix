30fe914a0147ca2094ff825d17f0f7c2560b183f
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2021-08-22 09:37:58 +0200

    Build stm-containers instead of the one from nixpkgs.
    As noted in
    https://github.com/noteed/start-servant/pull/11#discussion_r693383662,
    the package is marked as broken in nixpkgs. So we'll just pin it (and
    some of its unavailable dependencies) and build it ourselves.
    
    This has been done in nix/stm-containers.nix.
    
    @noteed
    This fix should suffice for using stm-containers.
    
    I had to also add `containers` to the cabal file since it was needed
    from your changes in #11; maybe you forgot to stage that change?

---
+++ nix/stm-containers.nix
@@ -0,0 +1,26 @@
+self: super:
+let
+
+  lib = super.lib;
+
+  inherit (import ./sources.nix) gitignore stm-containers stm-hamt focus; 
+
+  inherit (import gitignore { inherit lib; }) gitignoreSource;
+
+  stmContainersOverrides = selfh: superh: {
+    stm-containers =
+      selfh.callCabal2nix "stm-containers" stm-containers { };
+    # the following are the dependencies of stm-containers. 
+    stm-hamt =
+      selfh.callCabal2nix "stm-hamt" stm-hamt { };
+    focus =
+      selfh.callCabal2nix "focus" focus { };
+
+  };
+
+in {
+  haskellPackages = super.haskellPackages.override (old: {
+    overrides =
+      lib.composeExtensions (old.overrides or (_: _: { })) stmContainersOverrides;
+  });
+}
