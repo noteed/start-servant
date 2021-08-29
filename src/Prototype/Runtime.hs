21ac194be7be95e2d31ff133b33d327a19277ccd
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2021-08-29 16:44:56 +0200

    Runtime: with modes.
    Introducing the runtime and configuration. An application should start
    with a configuration; and the attempt to construct a Runtime in a
    suitable mode (possibly indicated using arguments); and start things
    up.
    
    Modes can be Stm or Postgres; and the right "Handler" will be used in
    the runtime based on the mode.
    
    This is again type-safe; and mixups will be easily avoided.
    
    This gives us a pretty good starting ground for being able to
    experiment with multiple storage systems underneath.
    
    Following this; we'll start constructing generalised ways for
    manipulating stored data; along with ACL.

--- src/Prototype/Runtime.hs
+++ src/Prototype/Runtime.hs
@@ -1,17 +1,90 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE
+    KindSignatures
+  , DataKinds
+  , TypeFamilies
+#-}
 module Prototype.Runtime
-  () where
+  ( -- * Configuration
+    Conf(..)
+  , cAppName
+  , cLogLevel
+  , AppMode(..)
+  , AppName(..)
+  , showAppName
+  , unAppName
+  , ModeStorage
+  -- * Runtime 
+  , Runtime(..)
+  , rConf
+  , rStorage
+  , rLogger
+  -- * Control
+  , AppM(..)
+  ) where
 
+import           Control.Lens
 import           Control.Monad.Log             as L
+import qualified Data.String                   as Str
+import qualified Data.Text                     as T
 import           Prelude                 hiding ( Handle )
-import           Prototype.Runtime.StmDatabase    as Db
+import           Prototype.Runtime.Errors
+import           Prototype.Runtime.StmDatabase as Db
 
-newtype Conf = Conf
-  { _cAppName :: Text
+-- | An application name: lets us group logging etc. with @/@ as separators.
+newtype AppName = AppName { _unAppName :: [Text] }
+                deriving (Eq, Show, Semigroup, Monoid) via [Text]
+
+-- | Reverse of the IsString instance (below)
+showAppName (AppName envs) = T.intercalate "/" envs
+
+makeLenses ''AppName
+
+-- | Take any string; split at @/@; and use it as the AppName.
+instance IsString AppName where
+  fromString = AppName . T.splitOn "/" . T.pack
+
+data Conf = Conf
+  { _cAppName  :: AppName -- ^ Application name 
+  , _cLogLevel :: L.Level -- ^ The logging level 
   }
   deriving (Eq, Show)
 
+makeLenses ''Conf
+
+data AppMode = Stm | Postgres
+             deriving (Eq, Show)
+
+-- | A Type level function that gives us the kind of storage
+-- based on the mode of the application.
+type family ModeStorage (mode :: AppMode) :: Type where
+  ModeStorage 'Stm = Handle
+  -- Later: added as an example 
+  -- ModeStorage 'Postres = ConnectionPool 
+
 -- | STM based runtime 
-data StmRuntime = StmRuntime
-  { _srConf    :: Conf
-  , _srStorage :: Handle
+data Runtime (mode :: AppMode) = Runtime
+  { _rConf    :: Conf -- ^ Original configuration with which the application was started 
+  , _rStorage :: ModeStorage mode -- ^ The actual storage 
+  , _rLogger  :: L.Logger AppName -- ^ The Logger 
   }
+
+makeLenses ''Runtime
+
+-- | The command centre: the MTL stack that processes all our computations
+newtype AppM (mode :: AppMode) a
+  = AppM { runAppM :: ReaderT (Runtime mode) (ExceptT RuntimeErr IO) a
+         }
+  deriving ( Functor
+           , Applicative
+           , Monad
+           , MonadIO
+           , MonadReader (Runtime mode)
+           , MonadError RuntimeErr
+           )
+
+instance L.MonadLog AppName (AppM mode) where
+  askLogger = asks _rLogger
+  localLogger f = local $ over rLogger f
