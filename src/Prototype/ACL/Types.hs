84dd9b93919d2e4c8fc2eda06f0a42ac1f957ab0
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2021-08-29 13:06:35 +0200

    Start with the ACL module(s).
    @noteed, as you pointed out in your email, I'm taking some ideas from
    the post: https://tailscale.com/blog/rbac-like-it-was-meant-to-be/

---
+++ src/Prototype/ACL/Types.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE
+    DerivingVia
+  , DeriveGeneric
+  , DeriveAnyClass
+#-}
+{-# LANGUAGE TemplateHaskell #-}
+module Prototype.ACL.Types
+  ( GroupId(..)
+  , unGroupId
+  -- * Permissions over tags.
+  , TagGrant(..)
+
+  -- * Tags
+  , Tag(..)
+  , unTag
+  , TagRels
+  ) where
+
+import           Control.Lens
+import           Data.Aeson
+import qualified Prototype.Types.NonEmptyText  as NE
+import           Servant.API                    ( FromHttpApiData
+                                                , ToHttpApiData
+                                                )
+
+ -- | ID of a group.
+newtype GroupId = GroupId { _unGroupId :: NE.NonEmptyText }
+                deriving ( Eq
+                         , Show
+                         , Ord
+                         , ToJSON
+                         , FromJSON
+                         , ToHttpApiData
+                         , FromHttpApiData
+                         ) via NE.NonEmptyText
+
+makeLenses ''GroupId
+
+-- | A grant is a specific permission given over a tag.
+data TagGrant =
+  TagRead -- ^ Read all objects tagged with a particular tag
+  | TagWrite -- ^ Write all objects tagged with a particular tag
+  | TagOwn  -- ^ Own a tag itself; owning a tag gives a grantee all permissions over the tagged objects
+            -- with the particular tag.
+  deriving (Eq, Show, Generic, Ord, Hashable)
+  deriving anyclass (ToJSON, FromJSON, ToJSONKey, FromJSONKey)
+
+-- | A Tag, on the other hand, is an arbitrary identifier that can be
+-- attached to resources. The combination of a tag and a grant define the ACL of the resource.
+newtype Tag = Tag { _unTag :: Text }
+            deriving (Eq, Show, ToJSON, FromJSON, Ord, IsString) via Text
+
+makeLenses ''Tag
+
+{- $tagRels A set of tags and the permissions the grantee has over them.
+
+  For example, if a grantee owns the tags "database" & "engineering", but can read
+  objects tagged "report"; we'd have:
+
+  @
+  TagOwn: [ "database", "engineering" ]
+  TagRead: [ "report" ]
+  @
+-}
+type TagRels = Map TagGrant (Set Tag)
---
+++ src/Prototype/Grantee/Group.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Prototype.Grantee.Group
+  (
+    -- * Groups
+    Group(..)
+  , gId
+  , gName
+  , gTags
+  ) where
+
+import           Control.Lens
+import           Prototype.ACL
+import qualified Prototype.ACL.Types           as ACLT
+
+-- | A group: a collection of users.
+data Group = Group
+  { _gId   :: ACLT.GroupId -- ^ ID of the Group, IDs must be unique and preferably generated.
+  , _gName :: Maybe Text -- ^ Name of the Group. Names are only used for user friendliness.
+  , _gTags :: ACLT.TagRels -- ^ Set of tags owned by the group.
+  }
+  deriving (Eq, Show, Ord)
+
+makeLenses ''Group
+
+{- | A group is a grantee:
+
+1. A group can own tags.
+
+2. A group, in essence, is in itself.
+-}
+instance Grantee Group where
+  granteeTags = _gTags
