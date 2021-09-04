0f19ee048201d26eeb37dce8c83939edcbc99a86
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2021-09-04 11:20:25 +0200

    Add booting functions: bootStm for now.

--- src/Prototype/Runtime.hs
+++ src/Prototype/Runtime.hs
@@ -11,6 +11,7 @@ module Prototype.Runtime
     Conf(..)
   , cAppName
   , cLogLevel
+  , cServerPort
   , AppMode(..)
   , AppName(..)
   , showAppName
@@ -18,11 +19,16 @@ module Prototype.Runtime
   , ModeStorage
   -- * Runtime
   , Runtime(..)
+  , StmRuntime
+  , PostgresRuntime
   , rConf
   , rStorage
   , rLogger
   -- * Control
   , AppM(..)
+  -- * Booting in specific modes.
+  -- TODO: add postgres mode in the future.
+  , bootStm
   ) where
 
 import           Control.Lens
@@ -37,6 +43,9 @@ import           Prototype.Runtime.StmDatabase as Db
 newtype AppName = AppName { _unAppName :: [Text] }
                 deriving (Eq, Show, Semigroup, Monoid) via [Text]
 
+instance TextShow AppName where
+  showb = showb . showAppName
+
 -- | Reverse of the IsString instance (below)
 showAppName (AppName envs) = T.intercalate "/" envs
 
@@ -47,12 +56,19 @@ instance IsString AppName where
   fromString = AppName . T.splitOn "/" . T.pack
 
 data Conf = Conf
-  { _cAppName  :: AppName -- ^ Application name
-  , _cLogLevel :: L.Level -- ^ The logging level
+  { _cAppName    :: AppName -- ^ Application name
+  , _cLogLevel   :: L.Level -- ^ The logging level
+  , _cServerPort :: Int -- ^ The port number to run the server on
   }
   deriving (Eq, Show)
 
 makeLenses ''Conf
+
+instance Default Conf where
+  def = Conf { _cAppName    = "start-servant"
+             , _cLogLevel   = L.levelDebug
+             , _cServerPort = 7249
+             }
 
 data AppMode = Stm | Postgres
              deriving (Eq, Show)
@@ -73,6 +89,10 @@ data Runtime (mode :: AppMode) = Runtime
 
 makeLenses ''Runtime
 
+-- | Type synonym for convenience
+type StmRuntime = Runtime 'Stm
+type PostgresRuntime = Runtime 'Postgres
+
 -- | The command centre: the MTL stack that processes all our computations
 newtype AppM (mode :: AppMode) a
   = AppM { runAppM :: ReaderT (Runtime mode) (ExceptT RuntimeErr IO) a
@@ -88,3 +108,20 @@ newtype AppM (mode :: AppMode) a
 instance L.MonadLog AppName (AppM mode) where
   askLogger = asks _rLogger
   localLogger f = local $ over rLogger f
+
+-- | Boot in the Stm storage mode.
+bootStm :: MonadIO m => Conf -> m (Either RuntimeErr StmRuntime)
+bootStm _rConf@Conf {..} = do
+  -- Start off by creating a logger first; we may want to log steps in the boot process.
+  _rLogger <- createLogger
+  L.runLogT' _rLogger . bootEnv $ do
+    -- Instantiate storage
+    L.info "Instantiating storage"
+    _rStorage <- liftIO Db.newHandle
+    pure . Right $ Runtime { .. }
+ where
+  bootEnv      = L.localEnv (<> "Boot")
+  createLogger = L.makeDefaultLogger L.simpleTimeFormat
+                                     (L.LogStdout 1024)
+                                     _cLogLevel
+                                     _cAppName
