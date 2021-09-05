c1a178f9afd0eeb1c110971b4c8ddad381580e70
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2021-09-05 11:06:22 +0200

    Added a TH level function that lets us get module information.
    @noteed this will help us with https://github.com/noteed/start-servant/issues/2
    
    Basically, each page can just generate this value at runtime; and we
    can ofcourse use the root repo. URL to also link these pages to GH.
    
    How this works with GH branches etc. remains to be seen; but I believe
    we can also compute that if we have the git environment available to
    us: GH uses the branch name and the commit hashes to point to URL
    "blobs".
    
    But this will get us there.

---
+++ src/Prototype/Module.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+{- |
+
+Module: Prototype.Module 
+Description: Get information about current module, etc.
+
+This is a slightly improved version of
+<https://stackoverflow.com/a/5679470/1360368 this> SO answer. 
+
+-}
+module Prototype.Module (moduleOf) where
+
+import qualified Data.Text as T 
+import Language.Haskell.TH
+
+-- | Get the name of the module. 
+  
+moduleOf :: Name -> Maybe Text
+moduleOf = dropLastToken . show 
+
+dropLastToken :: Text -> Maybe Text
+dropLastToken =
+   fmap (T.intercalate "." ) . initMay . T.splitOn "." 
+  
