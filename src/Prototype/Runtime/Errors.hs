95e31c9713325a3c7c59be6f5d68bdfc28864788
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2021-09-11 09:31:10 +0200

    Improve Errors documentation with an example.
    @noteed https://github.com/noteed/start-servant/pull/14#discussion_r704679484

--- src/Prototype/Runtime/Errors.hs
+++ src/Prototype/Runtime/Errors.hs
@@ -1,6 +1,32 @@
 {-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE GADTs #-}
+{- |
+Module: Prototype.Runtime.Errors
+Description: Custom errors and support for throwing these errors in some monad.
+
+We introduce RuntimeErr's that can be used to wrap either errors that are instances of `IsRuntimeErr`; or exceptions that are otherwise not handled.
+These errors then get mapped to Servant errors.
+
+Consider the following example:
+
+@
+data AuthErr = LoginFailed Text | PermissionDenied Text
+             deriving Show
+
+instance IsRuntimeErr AuthErr where
+
+  httpStatus = \case
+    LoginFailed{} -> HTTP.unauthorized401
+    PermissionDenied{} -> HTTP.forbidden403
+
+  userMessage = Just . \case
+    LoginFailed msg -> msg
+    PermissionDenied msg -> msg
+
+@
+
+-}
 module Prototype.Runtime.Errors
   ( RuntimeErr(..)
   , IsRuntimeErr(..)
@@ -57,3 +83,6 @@ instance IsRuntimeErr RuntimeErr where
   displayErr = \case
     KnownErr e -> displayErr e
     RuntimeException e -> T.unwords [ "RuntimeException" , show e, T.pack $ displayException e ]
+    
+
+    
