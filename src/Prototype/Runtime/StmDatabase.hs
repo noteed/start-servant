18b978e5f7c548745191e863d377f76ff6e71282
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2021-09-18 09:36:49 +0200

    Format StmDatabase module.
    @noteed I've formatted this module with Brittany; which I personally
    prefer.
    But Brittany is known to be pretty opinionated; however, I've seen it
    consistently achieves the best results. Brittany's opionated nature
    also means that the same piece of code results in the same block of
    code no matter who the programmer might be.
    
    Let me know if this is fine for you; or we need to look into other
    formatters.

--- src/Prototype/Runtime/StmDatabase.hs
+++ src/Prototype/Runtime/StmDatabase.hs
@@ -3,46 +3,60 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE RecordWildCards #-}
 
-module Prototype.Runtime.StmDatabase ( Handle(..)
-                          , newHandle
-                          , apply
-                          , getCounter, bumpCounter
-                          , getProfiles , getProfile
-                          , newCounter
-                          , login
-                          , getLoggedInProfile
-                          , getProfileAndLists
-                          , getProfileAndList
-                          , getSessions
-                          , getAllTodoLists
-                          ) where
+module Prototype.Runtime.StmDatabase
+  ( Handle(..)
+  , newHandle
+  , apply
+  , getCounter
+  , bumpCounter
+  , getProfiles
+  , getProfile
+  , newCounter
+  , login
+  , getLoggedInProfile
+  , getProfileAndLists
+  , getProfileAndList
+  , getSessions
+  , getAllTodoLists
+  ) where
 
-import Prototype.Types.Secret
-import Prelude hiding (Handle, toList)
-import Data.List (nub, sort)
-import Data.Maybe (catMaybes)
-import Control.Concurrent.STM (atomically, newTVar, readTVar, writeTVar, STM, TVar)
-import ListT (toList)
-import qualified StmContainers.Map as STM (Map)
-import qualified StmContainers.Map as STM.Map
+import           Control.Concurrent.STM         ( STM
+                                                , TVar
+                                                , atomically
+                                                , newTVar
+                                                , readTVar
+                                                , writeTVar
+                                                )
+import           Data.List                      ( nub
+                                                , sort
+                                                )
+import           Data.Maybe                     ( catMaybes )
+import           ListT                          ( toList )
+import           Prelude                 hiding ( Handle
+                                                , toList
+                                                )
+import           Prototype.Types.Secret
+import qualified StmContainers.Map             as STM
+                                                ( Map )
+import qualified StmContainers.Map             as STM.Map
 
-import qualified Prototype.Data.Examples as Examples
-import Prototype.Types
+import qualified Prototype.Data.Examples       as Examples
+import           Prototype.Types
 
 
 --------------------------------------------------------------------------------
 
 data Handle = Handle
-  { hCounter :: TVar Counter
-  , hSessions :: TVar [Session]
+  { hCounter            :: TVar Counter
+  , hSessions           :: TVar [Session]
     -- ^ This is only to keep track of logged in users. It means that in
     -- addition of having a User set in a cookie, the corresponding session
     -- must be present here.
-  , hUsers :: TVar [(Secret '[] Text, Profile)]
+  , hUsers              :: TVar [(Secret '[] Text, Profile)]
     -- ^ Password, and User. Those are real users. We don't store the password
     -- in a specific data type to avoid manipulating it and risking sending it
     -- over the wire.
-  , hTodoLists :: STM.Map TodoListId TodoList
+  , hTodoLists          :: STM.Map TodoListId TodoList
   , hNamespaceTodoLists :: STM.Map Namespace [TodoListId]
     -- ^ Associates namespaces to all their Todo lists.
   }
@@ -52,12 +66,12 @@ data Handle = Handle
 -- | Generate a new STM based storage.
 newHandle :: MonadIO m => m Handle
 newHandle = liftIO . atomically $ do
-  hCounter <- newCounter
-  hSessions <- newSessions
-  hUsers <- newUsers
-  hTodoLists <- newTodoLists
+  hCounter            <- newCounter
+  hSessions           <- newSessions
+  hUsers              <- newUsers
+  hTodoLists          <- newTodoLists
   hNamespaceTodoLists <- newNamespaceTodoLists
-  return Handle {..}
+  return Handle { .. }
 
 apply :: Handle -> Operation -> STM ()
 apply h BumpCounter = bumpCounter h
@@ -66,8 +80,7 @@ apply h BumpCounter = bumpCounter h
 --------------------------------------------------------------------------------
 newCounter = newTVar (Counter 1)
 
-getCounter h =
-  readTVar (hCounter h)
+getCounter h = readTVar (hCounter h)
 
 bumpCounter h = do
   Counter i <- readTVar (hCounter h)
@@ -83,13 +96,12 @@ getProfiles h = do
 getProfile h namespace_ = do
   users <- readTVar (hUsers h)
   return (lookup' users)
-  where
+ where
 
   lookup' profiles = case filter f profiles of
     [(_, p)] -> Just p
-    _ -> Nothing
-  f (_, profile) =
-    namespace (profile :: Profile) == namespace_
+    _        -> Nothing
+  f (_, profile) = namespace (profile :: Profile) == namespace_
 
 getProfileAndLists :: Handle -> Namespace -> STM (Maybe (Profile, [TodoList]))
 getProfileAndLists h namespace = do
@@ -100,7 +112,8 @@ getProfileAndLists h namespace = do
       return (Just (profile, lists))
     Nothing -> return Nothing
 
-getProfileAndList :: Handle -> Namespace -> Text -> STM (Maybe (Profile, TodoList))
+getProfileAndList
+  :: Handle -> Namespace -> Text -> STM (Maybe (Profile, TodoList))
 getProfileAndList h namespace listname = do
   mprofile <- getProfile h namespace
   case mprofile of
@@ -108,7 +121,7 @@ getProfileAndList h namespace listname = do
       mlist <- getTodoList h namespace listname
       case mlist of
         Just list' -> return (Just (profile, list'))
-        Nothing -> return Nothing
+        Nothing    -> return Nothing
     Nothing -> return Nothing
 
 
@@ -126,7 +139,7 @@ getTodoLists :: Handle -> Namespace -> STM [TodoList]
 getTodoLists h namespace = do
   mids <- STM.Map.lookup namespace (hNamespaceTodoLists h)
   case mids of
-    Nothing -> return []
+    Nothing  -> return []
     Just ids -> do
       mls <- mapM (\i -> STM.Map.lookup i (hTodoLists h)) ids
       return (catMaybes mls)
@@ -136,7 +149,7 @@ getTodoList h namespace listname = do
   lists <- getTodoLists h namespace
   pure $ case filter ((listname ==) . tlName) lists of
     [list'] -> Just list'
-    _ -> Nothing
+    _       -> Nothing
 
 newNamespaceTodoLists = do
   m <- STM.Map.new
@@ -184,36 +197,36 @@ getLoggedInProfile h user = do
   sessions <- getSessions h
   case lookupSession user sessions of
     Nothing -> return Nothing
-    Just _ -> do
+    Just _  -> do
       profiles <- getUsers h
       return $ lookupProfile user profiles
 
 --------------------------------------------------------------------------------
 -- | Convert the submitted login Credentials to a Profile.
-authenticateProfile :: Credentials -> [(Secret '[] Text, Profile)] -> Maybe Profile
+authenticateProfile
+  :: Credentials -> [(Secret '[] Text, Profile)] -> Maybe Profile
 authenticateProfile credentials profiles = case filter f profiles of
   [(_, p)] -> Just p
-  _ -> Nothing
-  where
+  _        -> Nothing
+ where
   f (pw, profile) =
-    namespace (profile :: Profile) == username (credentials :: Credentials) &&
-    pw =:= password (credentials :: Credentials)
+    namespace (profile :: Profile)
+      ==  username (credentials :: Credentials)
+      &&  pw
+      =:= password (credentials :: Credentials)
 
 -- Convert a User (taken from a signed cookie) to a Profile.
 lookupProfile :: User -> [(Secret '[] Text, Profile)] -> Maybe Profile
 lookupProfile user profiles = case filter f profiles of
   [(_, p)] -> Just p
-  _ -> Nothing
-  where
-  f (_, profile) =
-    namespace (profile :: Profile) == username (user :: User)
+  _        -> Nothing
+ where
+  f (_, profile) = namespace (profile :: Profile) == username (user :: User)
 
 -- In addition of `lookupProfile`, we can call this function to make sure a
 -- session was created.
 lookupSession :: User -> [Session] -> Maybe Session
 lookupSession user sessions = case filter f sessions of
   [s] -> Just s
-  _ -> Nothing
-  where
-  f session =
-    username (session :: Session) == username (user :: User)
+  _   -> Nothing
+  where f session = username (session :: Session) == username (user :: User)
